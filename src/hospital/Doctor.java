9b56e9eb375140b87cc132bb53c0fbb550ff032b
MomnaRasheed <momnarasheed2006@gmail.com>
2026-05-16 10:04:33 +0500

    create Doctor.java

---
+++ src/hospital/Doctor.java
@@ -0,0 +1,39 @@
+package hospital;
+
+public class Doctor extends Person{                        //inheritance
+private String specialization;                             //Encapsulation(private fields)
+private double fee;
+public String getSpecialization() {
+	return specialization;
+}
+public void setSpecialization(String Specialization) {
+	this.specialization=Specialization;
+	
+}
+public double getFee() {                                 //Encapsulaton(getter,setters)
+	return fee;
+}
+public void setFee(double fee) {
+	this.fee=fee;
+}
+public Doctor(String name,int Id) {                     //overloading constructor 1
+setName(name);
+setId(Id);
+}
+public Doctor(String name,int id, String Specialization,double fee) {    //overloading constructor 2
+	this.specialization=Specialization;
+	this.fee=fee;
+}
+@Override                                                    //override base classmethod(getRole)
+public String getRole() {
+	return "Doctor";
+	
+}
+@Override                                                    //override base class method(displayInfo)
+public void displayInfo() {
+	System.out.println("specialization: " + specialization);
+	System.out.println("Consultation Fee: " + fee);
+}
+
+}
+
